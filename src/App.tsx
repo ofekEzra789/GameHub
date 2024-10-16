eb2c3750a6f13c0c9d4655f331d6d323ee1f7d8b
ofek <ofekofek789@gmail.com>
2024-10-16 12:14:23 +0300

    Fetch the genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import {Grid, GridItem, Show} from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar.tsx";
 import GameGrid from "./components/GameGrid.tsx";
+import GenreList from "./components/GenreList.tsx";
 
 function App() {
-  return <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`
-  }}>
-        <GridItem area='nav'>
-            <NavBar />
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above={"lg"}>
+        <GridItem area="aside">
+          <GenreList />
         </GridItem>
-      <Show above={'lg'}>
-          <GridItem area='aside'>Aside</GridItem>
       </Show>
-        <GridItem area='main'>
-            <GameGrid />
-        </GridItem>
-  </Grid>
-
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
