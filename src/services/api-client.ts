d5ec60612543aedf9f0d20f39181dace65466626
ofek <ofekofek789@gmail.com>
2024-10-21 01:21:29 +0300

    Hide api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '9049b24aa33d41de95748666b1a3153c'
+        key: import.meta.env.VITE_API_KEY
     }
 })
