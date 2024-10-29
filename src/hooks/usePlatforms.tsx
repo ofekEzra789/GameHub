2234cbf4ebb655e1dfd893a0f3fee24ccfaedc50
ofek <ofekofek789@gmail.com>
2024-10-29 21:19:57 +0200

    Refactoring: create a reusable Api client

--- src/hooks/usePlatforms.tsx
+++ src/hooks/usePlatforms.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { FetchResponse } from "../services/api-client.ts"
-import apiClient from "../services/api-client";
+import APIClient from "../services/api-client";
 import platforms from "../data/platforms";
+
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
 export interface Platform {
   id: number;
@@ -12,12 +13,9 @@ export interface Platform {
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Platform>>("/platforms/lists/parents")
-        .then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24h
-    initialData: { count: platforms.length, results: platforms }
+    initialData: { count: platforms.length, results: platforms },
   });
 
 export default usePlatforms;
